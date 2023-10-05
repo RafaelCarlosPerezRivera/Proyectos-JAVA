e1cb5d7eb8fb5bea9eebeff2a43bb4cbb0737c29
RafaelCarlosPerezRivera <145059289+RafaelCarlosPerezRivera@users.noreply.github.com>
2023-10-05 22:38:08 +0200

    Add files via upload

---
+++ primer proyecto/src/POO/uso_coche.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class uso_coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//instanciamos una clase. Ejemplarizamos una clase
+		
+		coche Renault=new coche();
+		
+			
+			System.out.println(Renault.dime_ruedas() + " y " + Renault.dime_color());
+		
+		
+		
+	}
+
+}
